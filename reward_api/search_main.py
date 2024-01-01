a9df388a9f03c67d6e7c6a945ecbcb7373b2d6a8
tototianhao <5173315+toto000888@user.noreply.gitee.com>
2024-01-01 13:03:44 +0800

    add api request

--- reward_api/search_main.py
+++ reward_api/search_main.py
@@ -1,4 +1,5 @@
 import os.path
+import shutil
 from dataclasses import asdict
 
 from reward_any_api import *
@@ -6,6 +7,10 @@ from reward_any_api import *
 root_path = os.path.dirname(os.path.dirname(__file__))
 aime_path = os.path.join(root_path, 'knowledge_base', 'aime', 'content')
 
+coupon_path = os.path.join(aime_path, 'coupon')
+cashback_path = os.path.join(aime_path, 'cashback')
+shutil.rmtree(coupon_path)
+shutil.rmtree(cashback_path)
 
 def save_json_to_file(file_name, json):
     with open(file_name, 'w') as file:
@@ -21,10 +26,9 @@ def cashback_request_n_page(num=None):
         print("cashback page num = ", page_number)
         response = cashback_search(global_pub_id, global_api_key, page_number=page_number)
         json_result = json.dumps(asdict(response), indent=2)
-        save_dir = os.path.join(aime_path, 'cashback')
-        if not os.path.exists(save_dir):
-            os.makedirs(save_dir)
-        save_json_to_file(os.path.join(save_dir, f'search_response_{page_number}'), json_result)
+        if not os.path.exists(cashback_path):
+            os.makedirs(cashback_path)
+        save_json_to_file(os.path.join(cashback_path, f'search_response_{page_number}.json'), json_result)
         page_number = response.pageNumber + 1
         has_next = response.nextPage
 
@@ -39,10 +43,9 @@ def coupon_request_n_page(num=None):
         print("coupon page num = ", page_number)
         response = coupon_search(global_pub_id, global_api_key, page_number=page_number)
         json_result = json.dumps(asdict(response), indent=2)
-        save_dir = os.path.join(aime_path, 'coupon')
-        if not os.path.exists(save_dir):
-            os.makedirs(save_dir)
-        save_json_to_file(os.path.join(save_dir, f'search_response_{page_number}'), json_result)
+        if not os.path.exists(coupon_path):
+            os.makedirs(coupon_path)
+        save_json_to_file(os.path.join(coupon_path, f'search_response_{page_number}.json'), json_result)
         page_number = response.pageNumber + 1
         total_page = response.totalPages
 
